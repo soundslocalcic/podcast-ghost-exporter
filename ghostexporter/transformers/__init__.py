845bae266475d27be4d639479b40a74c0170b426
Mark Steadman <mark@origin.fm>
2025-06-13 13:00:33 +0100

    Adds v5 transformer

--- ghostexporter/transformers/__init__.py
+++ ghostexporter/transformers/__init__.py
@@ -4,3 +4,12 @@ Convenience module for finding transformers by version number.
 .. currentmodule:: ghostexporter.transformers
 .. moduleauthor:: Mark Steadman <mark@soundslocal.co.uk>
 """
+
+from .v5 import Ghost5Transformer
+
+
+def get_transformer(version: str):
+    """Return the transformer class matching the given version number."""
+    return {
+        "5": Ghost5Transformer
+    }[version]
---
+++ ghostexporter/transformers/v5.py
@@ -0,0 +1,72 @@
+"""
+Contains the v5 Ghost data transformer.
+
+.. currentmodule:: ghostexporter.transformers.v5
+.. moduleauthor:: Mark Steadman <mark@soundslocal.co.uk>
+"""
+
+from datetime import datetime
+from .base import JSONTransformer
+import json
+
+
+class Ghost5Transformer(JSONTransformer):
+    """v5 Ghost data transformer."""
+
+    def transform_items(self, item_hook: callable):
+        """Create a Ghost document with metadata and item list."""
+        now = datetime.now()
+        timestamp = now.timestamp()
+
+        return {
+            "db": [
+                {
+                    "meta": {
+                        "exported_on": int(timestamp * 1000),
+                        "version": "5.125.0-0-g09f7924d"
+                    },
+                    "data": item_hook
+                }
+            ]
+        }
+
+    def get_lexical(self, item):
+        """Return a Lexical document from an item."""
+        return {
+            "root": {
+                "children": [
+                    {
+                        "type": "html",
+                        "html": item.description,
+                        "version": 1
+                    }
+                ],
+                "type": "root"
+            }
+        }
+
+    def transform_item(self, item):
+        """Transform a feed item into a JSON object."""
+        lexical = self.get_lexical(item)
+        return {
+            "posts": [
+                {
+                    "id": item.id,
+                    "slug": item.slug,
+                    "title": item.title,
+                    "html": item.description,
+                    "lexical": json.dumps(lexical),
+                    "type": "post",
+                    "status": "published",
+                    "visibility": "public",
+                    "created_at": item.created,
+                    "published_at": item.published
+                }
+            ],
+            "posts_authors": [
+                {
+                    "post_id": item.id,
+                    "author_id": "1"
+                }
+            ]
+        }
